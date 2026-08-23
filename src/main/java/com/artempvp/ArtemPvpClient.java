2b07b0652bda9e1f45e23b3ab0f2768ff0c64cf0
artemvershyhora2024-star <artemvershyhora2024@gmail.com>
2026-08-23 17:56:46 +0300

    Update ArtemPvpClient.java

--- src/main/java/com/artempvp/ArtemPvpClient.java
+++ src/main/java/com/artempvp/ArtemPvpClient.java
@@ -11,26 +11,29 @@ public class ArtemPvpClient implements ClientModInitializer {
     public static final String MOD_ID = "artempvp-client";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    // Объявляем все компоненты HUD
     public static TopBarHud topBarHud;
     public static PotionsHud potionsHud;
     public static MusicPlayerHud musicPlayerHud;
     public static CooldownsHud cooldownsHud;
     public static KeybindsHud keybindsHud;
     public static InventoryHud inventoryHud;
+    public static TargetHud targetHud;
 
     @Override
     public void onInitializeClient() {
-        LOGGER.info("ArtemPVP Client initialized with Drag & Drop HUD!");
+        LOGGER.info("ArtemPVP Client fully initialized with all PvP modules!");
 
-        // Инициализация координат
+        // Задаем начальные координаты для каждого окошка на экране
         topBarHud = new TopBarHud(10, 10);
         cooldownsHud = new CooldownsHud(10, 40);
         keybindsHud = new KeybindsHud(10, 150);
         potionsHud = new PotionsHud(180, 10);
         musicPlayerHud = new MusicPlayerHud(330, 10);
         inventoryHud = new InventoryHud(330, 95);
+        targetHud = new TargetHud(330, 175);
 
-        // Рендеринг всех элементов на экране
+        // Регистрируем отрисовку всех элементов интерфейса через менеджер
         HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
             for (var comp : HudManager.getComponents()) {
                 comp.render(drawContext, 0, 0, tickDelta);
