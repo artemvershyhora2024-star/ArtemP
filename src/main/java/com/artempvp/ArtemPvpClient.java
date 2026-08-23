597524963fd17eb08485dc51416de792a7f63307
artemvershyhora2024-star <artemvershyhora2024@gmail.com>
2026-08-23 17:47:55 +0300

    Update ArtemPvpClient.java

--- src/main/java/com/artempvp/ArtemPvpClient.java
+++ src/main/java/com/artempvp/ArtemPvpClient.java
@@ -2,6 +2,10 @@ package com.artempvp;
 
 import com.artempvp.hud.components.TopBarHud;
 import com.artempvp.hud.components.PotionsHud;
+import com.artempvp.hud.components.MusicPlayerHud;
+import com.artempvp.hud.components.CooldownsHud;
+import com.artempvp.hud.components.KeybindsHud;
+import com.artempvp.hud.components.InventoryHud;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import org.slf4j.Logger;
@@ -13,19 +17,31 @@ public class ArtemPvpClient implements ClientModInitializer {
 
     public static TopBarHud topBarHud;
     public static PotionsHud potionsHud;
+    public static MusicPlayerHud musicPlayerHud;
+    public static CooldownsHud cooldownsHud;
+    public static KeybindsHud keybindsHud;
+    public static InventoryHud inventoryHud;
 
     @Override
     public void onInitializeClient() {
         LOGGER.info("ArtemPVP Client initialized!");
 
-        // Инициализируем элементы HUD на экране
+        // Инициализируем все окна интерфейса
         topBarHud = new TopBarHud(10, 10);
-        potionsHud = new PotionsHud(200, 10); // Расположим правее верхней панели
+        cooldownsHud = new CooldownsHud(10, 40);
+        keybindsHud = new KeybindsHud(10, 150);
+        potionsHud = new PotionsHud(180, 10);
+        musicPlayerHud = new MusicPlayerHud(330, 10);
+        inventoryHud = new InventoryHud(330, 95);
 
-        // Регистрируем отрисовку
+        // Регистрируем их общую отрисовку на экране
         HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
             topBarHud.render(drawContext, 0, 0, tickDelta);
+            cooldownsHud.render(drawContext, 0, 0, tickDelta);
+            keybindsHud.render(drawContext, 0, 0, tickDelta);
             potionsHud.render(drawContext, 0, 0, tickDelta);
+            musicPlayerHud.render(drawContext, 0, 0, tickDelta);
+            inventoryHud.render(drawContext, 0, 0, tickDelta);
         });
     }
 }
