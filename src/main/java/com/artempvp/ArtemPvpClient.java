292983082c6d0c4db5a896431b517f802b03d7fa
artemvershyhora2024-star <artemvershyhora2024@gmail.com>
2026-08-23 18:08:37 +0300

    Update ArtemPvpClient.java

--- src/main/java/com/artempvp/ArtemPvpClient.java
+++ src/main/java/com/artempvp/ArtemPvpClient.java
@@ -5,7 +5,6 @@ import com.artempvp.hud.components.*;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,15 +19,15 @@ public class ArtemPvpClient implements ClientModInitializer {
     public static KeybindsHud keybindsHud;
     public static InventoryHud inventoryHud;
     public static TargetHud targetHud;
+    public static KeystrokesHud keystrokesHud; // Наша новая переменная Keystrokes
 
     @Override
     public void onInitializeClient() {
-        LOGGER.info("ArtemPVP Client fully initialized!");
+        LOGGER.info("ArtemPVP Client fully initialized with Keystrokes!");
 
-        // Регистрируем клавиши
         KeyBindingManager.registerKeys();
 
-        // Инициализируем HUD элементы
+        // Инициализируем координаты всех элементов HUD
         topBarHud = new TopBarHud(10, 10);
         cooldownsHud = new CooldownsHud(10, 40);
         keybindsHud = new KeybindsHud(10, 150);
@@ -36,15 +35,26 @@ public class ArtemPvpClient implements ClientModInitializer {
         musicPlayerHud = new MusicPlayerHud(330, 10);
         inventoryHud = new InventoryHud(330, 95);
         targetHud = new TargetHud(330, 175);
+        keystrokesHud = new KeystrokesHud(10, 235); // Размещаем Keystrokes под остальными панелями слева
 
-        // Рендеринг всех элементов на экране
+        // Рендеринг элементов с учетом состояния кнопок в меню
         HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
-            for (var comp : HudManager.getComponents()) {
-                comp.render(drawContext, 0, 0, tickDelta);
+            ArtemPvpClient.topBarHud.render(drawContext, 0, 0, tickDelta);
+            ArtemPvpClient.potionsHud.render(drawContext, 0, 0, tickDelta);
+            ArtemPvpClient.keybindsHud.render(drawContext, 0, 0, tickDelta);
+            ArtemPvpClient.inventoryHud.render(drawContext, 0, 0, tickDelta);
+            ArtemPvpClient.targetHud.render(drawContext, 0, 0, tickDelta);
+            ArtemPvpClient.keystrokesHud.render(drawContext, 0, 0, tickDelta); // Рендерим WASD
+
+            if (ArtemPvpMenuScreen.musicHudEnabled) {
+                ArtemPvpClient.musicPlayerHud.render(drawContext, 0, 0, tickDelta);
+            }
+            if (ArtemPvpMenuScreen.cooldownsHudEnabled) {
+                ArtemPvpClient.cooldownsHud.render(drawContext, 0, 0, tickDelta);
             }
         });
 
-      // Открытие нашего GUI по нажатию Right Shift
+        // Открытие GUI по нажатию Right Shift
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (KeyBindingManager.openMenuKey.wasPressed()) {
                 if (client.player != null) {
