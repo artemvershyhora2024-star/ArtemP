9dfe908455c7f01db67bb885952512a1cc4105b3
artemvershyhora2024-star <artemvershyhora2024@gmail.com>
2026-08-23 17:42:51 +0300

    Update ArtemPvpClient.java

--- src/main/java/com/artempvp/ArtemPvpClient.java
+++ src/main/java/com/artempvp/ArtemPvpClient.java
@@ -1,6 +1,7 @@
 package com.artempvp;
 
 import com.artempvp.hud.components.TopBarHud;
+import com.artempvp.hud.components.PotionsHud;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import org.slf4j.Logger;
@@ -10,20 +11,21 @@ public class ArtemPvpClient implements ClientModInitializer {
     public static final String MOD_ID = "artempvp-client";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    // Создаем экземпляр нашей верхней панели
     public static TopBarHud topBarHud;
+    public static PotionsHud potionsHud;
 
     @Override
     public void onInitializeClient() {
         LOGGER.info("ArtemPVP Client initialized!");
 
-        // Инициализируем HUD вверху экрана (координаты X=10, Y=10)
+        // Инициализируем элементы HUD на экране
         topBarHud = new TopBarHud(10, 10);
+        potionsHud = new PotionsHud(200, 10); // Расположим правее верхней панели
 
-        // Регистрируем отрисовку через Fabric API, чтобы HUD показывался в игре
+        // Регистрируем отрисовку
         HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
-            // Передаем параметры рендеринга в нашу верхнюю панель
             topBarHud.render(drawContext, 0, 0, tickDelta);
+            potionsHud.render(drawContext, 0, 0, tickDelta);
         });
     }
 }
