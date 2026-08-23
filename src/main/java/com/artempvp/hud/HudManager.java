638a78e190ff2eae4c8e2aa0b39a87cc980e72b5
artemvershyhora2024-star <artemvershyhora2024@gmail.com>
2026-08-23 17:56:12 +0300

    Create HudManager.java

---
+++ src/main/java/com/artempvp/hud/HudManager.java
@@ -0,0 +1,19 @@
+package com.artempvp.hud;
+
+import com.artempvp.ArtemPvpClient;
+import java.util.Arrays;
+import java.util.List;
+
+public class HudManager {
+    public static List<DraggableHudComponent> getComponents() {
+        return Arrays.asList(
+            ArtemPvpClient.topBarHud,
+            ArtemPvpClient.cooldownsHud,
+            ArtemPvpClient.keybindsHud,
+            ArtemPvpClient.potionsHud,
+            ArtemPvpClient.musicPlayerHud,
+            ArtemPvpClient.inventoryHud,
+            ArtemPvpClient.targetHud
+        );
+    }
+}
